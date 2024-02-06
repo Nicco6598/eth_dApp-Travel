727903a5e6b0064534a430aefcb94cdb2f5d2f65
Nicco6598 <nicco6598@gmail.com>
2024-02-06 13:02:55 +0100

    Cambi grafici, gestione viaggi disponibili, aggiunta receipt una volta andata a buon fine la transazione

--- src/components/ProductGallery.tsx
+++ src/components/ProductGallery.tsx
@@ -1,53 +1,64 @@
-import React, { useContext } from 'react'; // Aggiungi useContext
+import React, { useContext, useState } from 'react'; // Aggiungi useContext
 import '../../src/style.css';
 import ProductItem from './ProductItem';
 import { ProviderContext } from './ProviderContext';
 
-
-const products = [
-  {
-    id: 1,
-    name: 'Viaggio alle Maldive',
-    description: 'Esperienza indimenticabile nelle splendide isole delle Maldive.',
-    imageUrl: 'https://images.unsplash.com/photo-1514282401047-d79a71a590e8?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Immagine casuale da Unsplash
-    price: '0.01' // Prezzo in ETH
-  },
-  {
-    id: 2,
-    name: 'Avventura in Nuova Zelanda',
-    description: 'Scopri la natura incontaminata e le avventure estreme in Nuova Zelanda.',
-    imageUrl: 'https://images.unsplash.com/photo-1507699622108-4be3abd695ad?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Immagine casuale da Unsplash
-    price: '1.2' // Prezzo in ETH
-  },
-  {
-    id: 3,
-    name: 'Settimana negli Stati Uniti',
-    description: 'Scopri la bellezza mozzafiato degli Stati Uniti',
-    imageUrl: 'https://images.unsplash.com/photo-1485738422979-f5c462d49f74?q=80&w=2099&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Immagine casuale da Unsplash
-    price: '2.12' // Prezzo in ETH
-  },
-  {
-    id: 4,
-    name: 'Alla scoperta del Giappone',
-    description: 'Esperienza indementicabile nel Sol Levante',
-    imageUrl: 'https://images.unsplash.com/photo-1464817739973-0128fe77aaa1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Immagine casuale da Unsplash
-    price: '3.55' // Prezzo in ETH
-  }
+const initialProducts = [
+    {
+      id: 1,
+      name: 'Viaggio alle Maldive',
+      description: 'Esperienza indimenticabile nelle splendide isole delle Maldive.',
+      imageUrl: 'https://images.unsplash.com/photo-1514282401047-d79a71a590e8?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Immagine casuale da Unsplash
+      price: '0.01', // Prezzo in ETH
+      available: 1
+    },
+    {
+      id: 2,
+      name: 'Avventura in Nuova Zelanda',
+      description: 'Scopri la natura incontaminata e le avventure estreme in Nuova Zelanda.',
+      imageUrl: 'https://images.unsplash.com/photo-1507699622108-4be3abd695ad?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Immagine casuale da Unsplash
+      price: '1.2', // Prezzo in ETH
+      available: 3
+    },
+    {
+      id: 3,
+      name: 'Settimana negli Stati Uniti',
+      description: 'Scopri la bellezza mozzafiato degli Stati Uniti',
+      imageUrl: 'https://images.unsplash.com/photo-1485738422979-f5c462d49f74?q=80&w=2099&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Immagine casuale da Unsplash
+      price: '2.12', // Prezzo in ETH
+      available: 5
+    },
+    {
+      id: 4,
+      name: 'Alla scoperta del Giappone',
+      description: 'Esperienza indementicabile nel Sol Levante',
+      imageUrl: 'https://images.unsplash.com/photo-1464817739973-0128fe77aaa1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Immagine casuale da Unsplash
+      price: '3.55', // Prezzo in ETH
+      available: 2
+    }
 ];
+  const ProductGallery = () => {
+    const { provider } = useContext(ProviderContext);
+    const [products, setProducts] = useState(initialProducts);
 
-const ProductGallery = () => {
-  const { provider } = useContext(ProviderContext); 
+    const handlePurchase = (productId) => {
+      setProducts(currentProducts =>
+        currentProducts.map(product =>
+          product.id === productId ? { ...product, available: product.available - 1 } : product,
+        ),
+      );
+    };
 
-  return (
-    <div>
-      <h2 className="text-3xl font-bold my-4 mt-24 text-center">VIAGGI DISPONIBILI</h2>
-      <div className="grid grid-cols-2 grid-rows-3 lg:grid-cols-3 gap-2 lg:gap-5">
-        {products.map(product => (
-          <ProductItem key={product.id} product={product}/> 
-        ))}
+    return (
+      <div>
+        <h2 className="text-3xl font-bold my-4 mt-24 text-center">VIAGGI DISPONIBILI</h2>
+        <div className="grid grid-cols-2 grid-rows-3 lg:grid-cols-3 gap-2 lg:gap-5">
+          {products.map(product => (
+            <ProductItem key={product.id} product={product} onPurchase={handlePurchase} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default ProductGallery;
+  export default ProductGallery;
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,75 +1,142 @@
-import React, { useState, useContext } from 'react'; // Aggiungi useContext
+import React, { useState, useContext } from 'react';
 import { ethers } from 'ethers';
 import ContractABI from './ProductSaleABI';
-import { ProviderContext } from './ProviderContext'; // Importa ProviderContext
+import { ProviderContext } from './ProviderContext';
 
-const ProductItem = ({ product }) => {
-    const { provider } = useContext(ProviderContext); // Utilizza useContext per ottenere il provider
-    const contractAddress = '0xFA73c7c78392655ABa60FBFC004f31688a06Ef60';
-    const [txHash, setTxHash] = useState('');
-    const [txCompleted, setTxCompleted] = useState(false);
-    
-    const buyProduct = async () => {
-        if (!provider) {
-            console.error("Wallet non connesso!");
-            console.log("Provider in ProductItem:", provider);
-            return;
-        }
+const ProductItem = ({ product, onPurchase }) => {
+  const { provider } = useContext(ProviderContext);
+  const contractAddress = '0xFA73c7c78392655ABa60FBFC004f31688a06Ef60';
+  const [txHash, setTxHash] = useState('');
+  const [txCompleted, setTxCompleted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [transactionFailed, setTransactionFailed] = useState(false);
+  const [transactionInProgress, setTransactionInProgress] = useState(false);
 
-        const network = await provider.getNetwork();
-        console.log("Connesso alla rete:", network);
+  const buyProduct = async () => {
+    if (!provider) {
+      console.error("Wallet non connesso!");
+      return;
+    }
 
-        if (network.chainId !== 11155111) { // Chain ID per Sepolia Testnet
-            console.error("Non sei sulla rete Sepolia Testnet!");
-            return;
-        }
+    const network = await provider.getNetwork();
+    if (network.chainId !== 11155111) {
+      console.error("Non sei sulla rete Sepolia Testnet!");
+      return;
+    }
 
-        setTxHash('');
-        setTxCompleted(false);
+    if (product.available <= 0) {
+      console.error("Prodotto non disponibile!");
+      return;
+    }
 
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(contractAddress, ContractABI, signer);
+    setTxHash('');
+    setTxCompleted(false);
+    setTransactionFailed(false);
+    setTransactionInProgress(true);
 
-        try {
-            const tx = await contract.buyProduct({ value: ethers.utils.parseEther(product.price) });
-            setTxHash(tx.hash);
-            console.log(`Transazione inviata. Hash: ${tx.hash}`);
-            await tx.wait();
-            setTxCompleted(true);
-            console.log("Acquisto completato!");
-        } catch (error) {
-            console.error("Errore nell'acquisto del prodotto:", error);
-        }
-    };
+    try {
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, ContractABI, signer);
 
+      setTransactionInProgress(true); // Imposta "Transazione in corso..." prima di inviare la transazione
+
+      const tx = await contract.buyProduct({ value: ethers.utils.parseEther(product.price) });
+      setTxHash(tx.hash);
+      console.log(`Transazione inviata. Hash: ${tx.hash}`);
+
+      // Attendere che la transazione venga completata prima di mostrare il modal
+      await tx.wait();
+
+      setTxCompleted(true);
+      setTransactionInProgress(false);
+      console.log("Acquisto completato!");
+      onPurchase(product.id); // Notifica al genitore l'acquisto
+      setShowModal(true); // Mostra il modal solo dopo il completamento della transazione
+    } catch (error) {
+      console.error("Errore nell'acquisto del prodotto:", error);
+      setTransactionFailed(true);
+      setTransactionInProgress(false); // Imposta a false in caso di errore
+    }
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setTxHash('');
+    setTxCompleted(false);
+    setTransactionFailed(false);
+    setTransactionInProgress(false);
+  };
+
+  const formatTxHash = (hash) => {
+    const prefix = hash.slice(0, 10);
+    const suffix = hash.slice(-4);
     return (
-        <div className="mt-20 hover:scale-105 w-fit transition duration-300 ease-in-out transform lg:flex rounded-3xl shadow-[0px_15px_25px_15px_#cbd5e0] hover:shadow-[0px_10px_15px_5px_#a0aec0] mr-12 ml-12 ">
-            <div className="h-48 lg:w-48 object-contain flex-none bg-cover rounded-3xl text-center overflow-hidden" style={{ backgroundImage: `url(${product.imageUrl})` }} title={product.name}>
-            </div>
-            <div className="bg-gray rounded-b p-4 flex flex-col justify-between leading-normal">
-                <div className="mb-8">
-                    <div className="text-gray-900 font-bold text-xl mb-2">{product.name}</div>
-                    <p className="text-gray-700 text-base">{product.description}</p>
-                </div>
-                <div className="flex items-center justify-between">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xl font-bold text-black-700">{product.price} ETH</span>
-                    <button onClick={buyProduct} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-                        Acquista
-                    </button>
-                    {txHash && (
-                        <div className="text-sm mt-2">
-                            <p>Hash Transazione: {txHash}</p>
-                            {txCompleted ? (
-                                <p className="text-green-500">Transazione completata!</p>
-                            ) : (
-                                <p className="text-yellow-500">Transazione in corso...</p>
-                            )}
-                        </div>
-                    )}
-                </div>
-            </div>
-        </div>
+      <a href={`https://sepolia.etherscan.io/tx/${hash}`} target="_blank" rel="noopener noreferrer" className="underline text-blue-500">
+        {`${prefix}...${suffix}`}
+      </a>
     );
+  };
+
+  return (
+    <div className="mt-20 hover:scale-105 w-fit transition duration-300 ease-in-out transform lg:flex rounded-3xl shadow-[0px_15px_25px_15px_#cbd5e0] hover:shadow-[0px_10px_15px_5px_#a0aec0] mr-12 ml-12 ">
+      <div className="h-auto lg:w-48 object-contain flex-none bg-cover rounded-3xl text-center overflow-hidden" style={{ backgroundImage: `url(${product.imageUrl})` }} title={product.name}>
+      </div>
+      <div className="bg-gray rounded-b p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          <div className="text-gray-900 font-bold text-xl mb-2">{product.name}</div>
+          <p className="text-gray-700 text-base">{product.description}</p>
+          <p> ---------------  </p>
+          <p className="text-gray-700 font-bold text-base">Disponibili: {product.available}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xl font-bold text-black-700">{product.price} ETH</span>
+          {product.available > 0 ? (
+            <button onClick={buyProduct} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+              Acquista
+            </button>
+          ) : (
+            <button disabled className="bg-blue-500 text-white font-bold py-2 px-4 rounded-2xl opacity-50 cursor-not-allowed">
+              Esaurito
+            </button>
+          )}
+          {showModal && (
+            <div className="fixed inset-0 flex items-center justify-center z-50">
+              <div className="bg-white p-8 rounded-3xl shadow-lg lg:w-48" style={{ width: '520px' }}>
+                <div className="h-auto object-contain flex-none bg-cover rounded-3xl text-center overflow-hidden" style={{ backgroundImage: `url(${product.imageUrl})` }} title={product.name}></div>
+                {transactionInProgress ? (
+                  <div className="mt-6">
+                    <p className="font-semibold">Prodotto: {product.name}</p>
+                    <p className="font-semibold">Prezzo: {product.price} ETH</p>
+                    <p className="font-semibold text-blue-500">Stato Transazione: Transazione in corso...</p>
+                  </div>
+                ) : txCompleted ? (
+                  <div className="mt-6">
+                    <p className="font-semibold">Prodotto: {product.name}</p>
+                    <p className="font-semibold">Hash Transazione: {formatTxHash(txHash)}</p>
+                    <p className="font-semibold text-amber-500">Prezzo: {product.price}</p>
+                    <p className="font-semibold text-green-500">Stato Transazione: Completata</p>
+                  </div>
+                ) : transactionFailed ? (
+                  <div className="mt-6">
+                    <p className="font-semibold text-red-500">Transazione annullata o fallita.</p>
+                  </div>
+                ) : (
+                  <div className="mt-6">
+                    <p className="font-semibold">Prodotto: {product.name}</p>
+                    <p className="font-semibold">Hash Transazione: {formatTxHash(txHash)}</p>
+                  </div>
+                )}
+                <button onClick={closeModal} className="mt-4 bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-2xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                  Chiudi
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItem;
+
